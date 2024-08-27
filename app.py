751c37a225e0f6e1b498a8d847ece0f71312bbf0
Marlon Melo <marlonssmelo05@gmail.com>
2024-08-26 22:48:09 -0300

    New funcionalities

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, url_for, redirect
+import urllib.request, json
 
 app = Flask(__name__)
 
@@ -9,7 +10,9 @@ usuarios = [
     }
 ]
 
-frutas = []
+clientes = [
+
+]
 
 @app.route('/', methods=["GET", "POST"])
 def principal():
@@ -24,14 +27,23 @@ def principal():
     return render_template("index.html")
 
 
-@app.route('/sobre', methods=["GET", "POST"])
-def sobre():
-    if request.method == "POST":
-        if request.form.get("adicionar"):
-            frutas.append(request.form.get("adicionar"))
-            return render_template("sobre.html", frutas=frutas)
+@app.route('/filmes/<propiedade>', methods=['GET'])
+def filmes(propiedade):
+    if propiedade == "populares":
+        url = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=71449df069c07d2ed4c33c80027dd880"
+    elif propiedade == "kids":
+        url = "https://api.themoviedb.org/3/discover/movie?certification_country=US&certification.lte=G&sort_by=popularity.desc&api_key=71449df069c07d2ed4c33c80027dd880"
+    elif propiedade == "2010":
+        url = "https://api.themoviedb.org/3/discover/movie?primary_release_year=2010&sort_by=vote_average.desc&api_key=71449df069c07d2ed4c33c80027dd880"
+    elif propiedade == "drama":
+        url = "https://api.themoviedb.org/3/discover/movie?with_genres=18&sort_by=vote_average.desc&api_key=71449df069c07d2ed4c33c80027dd880"
+    elif propiedade == "tom_cruise":
+        url = "https://api.themoviedb.org/3/discover/movie?with_genres=878&with_cast=500&sort_by=vote_average.desc&api_key=71449df069c07d2ed4c33c80027dd880"
     else:
-        return render_template("sobre.html", frutas=frutas)
-
+        return "Invalid property specified.", 400
+    resposta = urllib.request.urlopen(url)
+    dados = resposta.read()
+    jsondata = json.loads(dados)
+    return render_template("filmes.html", filmes=jsondata['results'])
 if __name__ == "__main__":
     app.run(port=5000, host='localhost', debug=True)
